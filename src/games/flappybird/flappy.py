fd0158fa67fb578668df186c215f30dcc42ed6ae
Luca <luca-heinrich@gmx.de>
2022-02-07 11:54:34 +0100

    - added shell script to run flappybird

--- src/games/flappybird/flappy.py
+++ src/games/flappybird/flappy.py
@@ -1,3 +1,4 @@
+import os
 from itertools import cycle
 import random
 import sys
@@ -12,38 +13,41 @@ BASEY        = SCREENHEIGHT * 0.79
 # image, sound and hitmask  dicts
 IMAGES, SOUNDS, HITMASKS = {}, {}, {}
 
+ROOT_DIR = os.environ.get('FLAPPY_DIR', "")
+
+
 # list of all possible players (tuple of 3 positions of flap)
 PLAYERS_LIST = (
     # red bird
     (
-        'assets/sprites/redbird-upflap.png',
-        'assets/sprites/redbird-midflap.png',
-        'assets/sprites/redbird-downflap.png',
+        os.path.join(ROOT_DIR, 'assets/sprites/redbird-upflap.png'),
+        os.path.join(ROOT_DIR, 'assets/sprites/redbird-midflap.png'),
+        os.path.join(ROOT_DIR, 'assets/sprites/redbird-downflap.png'),
     ),
     # blue bird
     (
-        'assets/sprites/bluebird-upflap.png',
-        'assets/sprites/bluebird-midflap.png',
-        'assets/sprites/bluebird-downflap.png',
+        os.path.join(ROOT_DIR, 'assets/sprites/bluebird-upflap.png'),
+        os.path.join(ROOT_DIR, 'assets/sprites/bluebird-midflap.png'),
+        os.path.join(ROOT_DIR, 'assets/sprites/bluebird-downflap.png'),
     ),
     # yellow bird
     (
-        'assets/sprites/yellowbird-upflap.png',
-        'assets/sprites/yellowbird-midflap.png',
-        'assets/sprites/yellowbird-downflap.png',
+        os.path.join(ROOT_DIR, 'assets/sprites/yellowbird-upflap.png'),
+        os.path.join(ROOT_DIR, 'assets/sprites/yellowbird-midflap.png'),
+        os.path.join(ROOT_DIR, 'assets/sprites/yellowbird-downflap.png'),
     ),
 )
 
 # list of backgrounds
 BACKGROUNDS_LIST = (
-    'assets/sprites/background-day.png',
-    'assets/sprites/background-night.png',
+    os.path.join(ROOT_DIR, 'assets/sprites/background-day.png'),
+    os.path.join(ROOT_DIR, 'assets/sprites/background-night.png'),
 )
 
 # list of pipes
 PIPES_LIST = (
-    'assets/sprites/pipe-green.png',
-    'assets/sprites/pipe-red.png',
+    os.path.join(ROOT_DIR, 'assets/sprites/pipe-green.png'),
+    os.path.join(ROOT_DIR, 'assets/sprites/pipe-red.png'),
 )
 
 
@@ -62,24 +66,24 @@ def main():
 
     # numbers sprites for score display
     IMAGES['numbers'] = (
-        pygame.image.load('assets/sprites/0.png').convert_alpha(),
-        pygame.image.load('assets/sprites/1.png').convert_alpha(),
-        pygame.image.load('assets/sprites/2.png').convert_alpha(),
-        pygame.image.load('assets/sprites/3.png').convert_alpha(),
-        pygame.image.load('assets/sprites/4.png').convert_alpha(),
-        pygame.image.load('assets/sprites/5.png').convert_alpha(),
-        pygame.image.load('assets/sprites/6.png').convert_alpha(),
-        pygame.image.load('assets/sprites/7.png').convert_alpha(),
-        pygame.image.load('assets/sprites/8.png').convert_alpha(),
-        pygame.image.load('assets/sprites/9.png').convert_alpha()
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/0.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/1.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/2.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/3.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/4.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/5.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/6.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/7.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/8.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/9.png')).convert_alpha()
     )
 
     # game over sprite
-    IMAGES['gameover'] = pygame.image.load('assets/sprites/gameover.png').convert_alpha()
+    IMAGES['gameover'] = pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/gameover.png')).convert_alpha()
     # message sprite for welcome screen
-    IMAGES['message'] = pygame.image.load('assets/sprites/message.png').convert_alpha()
+    IMAGES['message'] = pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/message.png')).convert_alpha()
     # base (ground) sprite
-    IMAGES['base'] = pygame.image.load('assets/sprites/base.png').convert_alpha()
+    IMAGES['base'] = pygame.image.load(os.path.join(ROOT_DIR, 'assets/sprites/base.png')).convert_alpha()
 
     # sounds
     if 'win' in sys.platform:
@@ -87,11 +91,11 @@ def main():
     else:
         soundExt = '.ogg'
 
-    SOUNDS['die']    = pygame.mixer.Sound('assets/audio/die' + soundExt)
-    SOUNDS['hit']    = pygame.mixer.Sound('assets/audio/hit' + soundExt)
-    SOUNDS['point']  = pygame.mixer.Sound('assets/audio/point' + soundExt)
-    SOUNDS['swoosh'] = pygame.mixer.Sound('assets/audio/swoosh' + soundExt)
-    SOUNDS['wing']   = pygame.mixer.Sound('assets/audio/wing' + soundExt)
+    SOUNDS['die']    = pygame.mixer.Sound(os.path.join(ROOT_DIR, 'assets/audio/die') + soundExt)
+    SOUNDS['hit']    = pygame.mixer.Sound(os.path.join(ROOT_DIR, 'assets/audio/hit') + soundExt)
+    SOUNDS['point']  = pygame.mixer.Sound(os.path.join(ROOT_DIR, 'assets/audio/point') + soundExt)
+    SOUNDS['swoosh'] = pygame.mixer.Sound(os.path.join(ROOT_DIR, 'assets/audio/swoosh') + soundExt)
+    SOUNDS['wing']   = pygame.mixer.Sound(os.path.join(ROOT_DIR, 'assets/audio/wing') + soundExt)
 
     while True:
         # select random background sprites
---
+++ src/party-pi_startup_EXAMPLE.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -a
+source .env
+set +a
+
+python ./games/flappybird/flappy.py
